547d74600008992f5844d317c53c8e9b01cbff73
Helio <jhelioneto@hotmail.com>
2022-12-10 09:29:54 -0300

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+export default function Banner() {
+    return <Carousel>
+        <img src="/image1.png" />
+        <img src="/image2.png" />
+        <img src="/image3.png" />
+        <img src="/image4.png" />
+        <img src="/image5.png" />
+    </Carousel>
+}
